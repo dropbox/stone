20abf7d319ad383d34ef581a2943dbda75dbb21f
Ken Elkabany <kelkabany@dropbox.com>
2014-12-11 23:59:49 -0800

    Fixed when validation happens during encoding.

--- babelapi/generator/target/python/babel_serializers.py
+++ babelapi/generator/target/python/babel_serializers.py
@@ -61,40 +61,43 @@ class JsonEncoder(object):
         1. The passed in obj may not have been validated with data_type yet.
         2. If an object that should be a Struct was assigned to a field, its
            type has been validated, but the presence of all required fields
-           hasn't been..
+           hasn't been.
         3. If an object that should be a Union was assigned to a field, whether
            or not a tag has been set has not been validated.
         4. A list may have passed validation initially, but been mutated since.
         """
-        data_type.validate(obj)
         return json.dumps(cls._encode_helper(data_type, obj))
 
     @classmethod
-    def _encode_helper(cls, data_type, obj):
-        """Encodes a Babel data type into a JSON-compatible dict."""
+    def _encode_helper(cls, data_type, obj, validate_primitives=True):
+        """
+        Encodes a Babel data type into a JSON-compatible dict.
+
+        We skip validation of fields with primitive data types in structs and
+        unions because they've already been validated on assignment.
+        """
         if isinstance(data_type, dt.List):
             data_type.validate(obj)
-            return [cls._encode_helper(data_type.data_type, item) for item in obj]
+            return [cls._encode_helper(data_type.item_data_type, item)
+                    for item in obj]
         elif isinstance(data_type, dt.PrimitiveType):
+            if validate_primitives:
+                data_type.validate(obj)
             return cls._make_json_friendly(data_type, obj)
         elif isinstance(data_type, dt.Struct):
-            d = {}
             data_type.validate(obj)
-            for name, field_data_type  in data_type.data_type._fields_:
+            d = {}
+            for name, field_data_type  in data_type.definition._fields_:
                 val = getattr(obj, name)
                 if val is not None:
-                    d[name] = cls._encode_helper(field_data_type, val)
+                    d[name] = cls._encode_helper(field_data_type, val, False)
             return d
         elif isinstance(data_type, dt.Union):
             data_type.validate(obj)
-            field_data_type = data_type.data_type._fields_[obj._tag]
+            field_data_type = data_type.definition._fields_[obj._tag]
             if field_data_type:
                 if isinstance(field_data_type, (dt.Any, dt.Symbol)):
-                    # TODO(kelkabany): Perhaps a JSON-serializable Any field
-                    # should look like {tag: null}
                     return obj._tag
-                elif isinstance(field_data_type, dt.Any):
-                    return {obj._tag: None}
                 elif isinstance(field_data_type, dt.PrimitiveType):
                     val = getattr(obj, obj._tag)
                     return cls._make_json_friendly(field_data_type, val)
@@ -139,36 +142,37 @@ class JsonDecoder(object):
         if isinstance(data_type, dt.Struct):
             if strict:
                 for key in obj:
-                    if key not in data_type.data_type._field_names_:
+                    if key not in data_type.definition._field_names_:
                         raise dt.ValidationError("unknown field '%s'" % key)
-            o = data_type.data_type()
-            for name, field_data_type in data_type.data_type._fields_:
+            o = data_type.definition()
+            for name, field_data_type in data_type.definition._fields_:
                 if name in obj:
                     v = cls._decode_helper(field_data_type, obj[name], strict)
                     setattr(o, name, v)
             data_type.validate(o)
         elif isinstance(data_type, dt.Union):
-            o = data_type.data_type()
+            o = data_type.definition()
             if isinstance(obj, six.string_types):
                 # Variant is a symbol
                 tag = obj
-                if tag in data_type.data_type._fields_:
-                    val_data_type = data_type.data_type._fields_[tag]
+                if tag in data_type.definition._fields_:
+                    val_data_type = data_type.definition._fields_[tag]
                     if not isinstance(val_data_type, (dt.Any, dt.Symbol)):
                         raise dt.ValidationError("expected object for '%s', got symbol"
                                                  % tag)
                 else:
-                    if not strict and data_type.data_type._catch_all_:
-                        tag = data_type.data_type._catch_all_
+                    if not strict and data_type.definition._catch_all_:
+                        tag = data_type.definition._catch_all_
                     else:
                         raise dt.ValidationError("unknown tag '%s'" % tag)
                 getattr(o, 'set_' + tag)()
             elif isinstance(obj, dict):
                 # Variant is not a symbol
-                assert len(obj) == 1, 'only 1 key must be specified'
+                if len(obj) != 1:
+                    raise dt.ValidationError('expected 1 key, got %s', len(obj))
                 tag, val = obj.items()[0]
-                if tag in data_type.data_type._fields_:
-                    val_data_type = data_type.data_type._fields_[tag]
+                if tag in data_type.definition._fields_:
+                    val_data_type = data_type.definition._fields_[tag]
                     if isinstance(val_data_type, dt.Any):
                         getattr(o, 'set_' + tag)()
                     elif isinstance(val_data_type, dt.Symbol):
@@ -178,26 +182,26 @@ class JsonDecoder(object):
                         v = cls._decode_helper(val_data_type, val, strict)
                         setattr(o, tag, v)
                 else:
-                    if not strict and data_type.data_type._catch_all_:
-                        tag = data_type.data_type._catch_all_
+                    if not strict and data_type.definition._catch_all_:
+                        tag = data_type.definition._catch_all_
                         getattr(o, 'set_' + tag)()
                     else:
                         raise dt.ValidationError("unknown tag '%s'" % tag)
             else:
-                raise TypeError('expected str or dict, got %r'
-                                % dt.generic_type_name((obj)))
+                raise dt.ValidationError("expected string or object, got '%s'"
+                                         % dt.generic_type_name((obj)))
         elif isinstance(data_type, dt.List):
             if not isinstance(obj, list):
                 raise dt.ValidationError(
                     'expected list, got %s'
                     % dt.generic_type_name(obj)
                 )
-            return [cls._decode_helper(data_type.data_type, item, strict)
+            return [cls._decode_helper(data_type.item_data_type, item, strict)
                     for item in obj]
         elif isinstance(data_type, dt.PrimitiveType):
             return cls._make_babel_friendly(data_type, obj)
         else:
-            raise AssertionError('expected Struct or Union, got %r'
+            raise AssertionError('cannot handle type %r'
                                  % dt.generic_type_name(obj))
         return o
 
