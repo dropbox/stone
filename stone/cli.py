acba5705d6c40f088430b9ea163e897ebd933ac5
Stephen Cobbe <scobbe@dropbox.com>
2016-06-27 12:08:00 -0700

    Harmonize Swift generation to be more in line with Python generation structure.

--- stone/cli.py
+++ stone/cli.py
@@ -24,6 +24,7 @@ _builtin_generators = (
     'js_client',
     'python_types',
     'python_client',
+    'swift_types',
     'swift_client',
 )
 
--- stone/target/swift_helpers.py
+++ stone/target/swift_helpers.py
@@ -2,11 +2,43 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import pprint
 
+from stone.data_type import (
+    Boolean,
+    Bytes,
+    Float32,
+    Float64,
+    Int32,
+    Int64,
+    List,
+    String,
+    Timestamp,
+    UInt32,
+    UInt64,
+    Void,
+    is_list_type,
+    is_user_defined_type,
+    unwrap_nullable,
+)
 from .helpers import split_words
 
 # This file defines *stylistic* choices for Swift
 # (ie, that class names are UpperCamelCase and that variables are lowerCamelCase)
 
+
+_type_table = {
+    Boolean: 'Bool',
+    Bytes: 'NSData',
+    Float32: 'Float',
+    Float64: 'Double',
+    Int32: 'Int32',
+    Int64: 'Int64',
+    List: 'Array',
+    String: 'String',
+    Timestamp: 'NSDate',
+    UInt32: 'UInt32',
+    UInt64: 'UInt64',
+    Void: 'Void',
+}
 
 _reserved_words = {
     'description',
@@ -74,6 +106,20 @@ def fmt_func(name):
     return _format_camelcase(name)
 
 
+def fmt_type(data_type):
+    data_type, nullable = unwrap_nullable(data_type)
+
+    if is_user_defined_type(data_type):
+        result = '{}.{}'.format(fmt_class(data_type.namespace.name),
+                                fmt_class(data_type.name))
+    else:
+        result = _type_table.get(data_type.__class__, fmt_class(data_type.name))
+        
+        if is_list_type(data_type):
+            result = result + '<{}>'.format(fmt_type(data_type.data_type))            
+    
+    return result if not nullable else result + '?'
+
+
 def fmt_var(name):
     return _format_camelcase(name)
-
