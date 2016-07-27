15fe78b7819da442cd7132653441aef190f38a9d
Max Wittek <wimax@dropbox.com>
2016-07-27 14:44:58 -0700

    Fixed mypy-run.sh

--- mypy-run.sh
+++ mypy-run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -eux
 
-mypy --strict-optional --silent-imports --fast-parser ./stone
+EXCLUDE="(^example|ez_setup.py)"
+
+# Include all Python files registered in Git, that don't occur in $EXCLUDE.
+INCLUDE=$(git ls-files "$@" | grep '\.py$' | egrep -v "$EXCLUDE" | tr '\n' '\0' | xargs -0 | cat)
+
+mypy --strict-optional --silent-imports --fast-parser $INCLUDE
