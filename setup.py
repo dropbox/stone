577458e082a4a247f0296f41006e41176e7a0fb1
Matt Bogosian <mbogosian@dropbox.com>
2016-12-13 15:28:26 -0800

    Try to repro Travis CI/docutils 0.13.1 issue
    See:
    
    * https://github.com/travis-ci/travis-ci/issues/7028
    * https://sourceforge.net/p/docutils/bugs/302/
    * https://github.com/dropbox/stone/pull/28#discussion-diff-91844629L44

--- setup.py
+++ setup.py
@@ -2,62 +2,17 @@
 # where package_data is expected to be str and not unicode.
 from __future__ import absolute_import, division, print_function
 
-import sys
-
-try:
-    from ez_setup import use_setuptools
-    use_setuptools()
-except ImportError:
-    # Try to use ez_setup, but if not, continue anyway. The import is known
-    # to fail when installing from a tar.gz.
-    print('Could not import ez_setup', file=sys.stderr)
-
 from setuptools import setup
-
-install_reqs = [
-    'ply>=3.4',
-    'six>=1.3.0',
-    'typing>=3.5.2',
-]
-
-with open('LICENSE') as f:
-    LICENSE = f.read()
-
-with open('README.rst') as f:
-    README = f.read()
 
 dist = setup(
     name='stone',
     version='0.1',
-    description='Stone is an interface description language (IDL) for APIs.',
+    description='DESCRIPTION',
     author='Ken Elkabany',
     author_email='kelkabany@dropbox.com',
-    maintainer='Dropbox',
-    maintainer_email='dev-platform@dropbox.com',
     url='https://github.com/dropbox/stone',
-    install_requires=install_reqs,
-    license=LICENSE,
-    zip_safe=False,
-    packages=['stone',
-              'stone.lang',
-              'stone.target',
-              'stone.target.python_rsrc'],
-    long_description=README,
-    platforms=['CPython 2.7', 'CPython 3.4', 'CPython 3.5'],
-    entry_points={
-        'console_scripts': ['stone=stone.cli:main'],
-    },
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Code Generators',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    install_requires=[],
+    license='LICENSE',
+    packages=['stone'],
+    long_description='README',
 )
