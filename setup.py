2582bce70db128c303df3cc9da6ac85292cbc188
Matt Bogosian <mbogosian@dropbox.com>
2017-03-02 21:14:24 -0800

    Clean up repo
    - Add 3.6 to platforms and tests.
    - Add linter ignore comments to auto-generated Python files.
    - Clean up .gitignore.
    - Clean up PKG-INFO.
    - Rename doc/ -> docs/ for parity with dropbox-sdk-python.

--- setup.py
+++ setup.py
@@ -15,8 +15,8 @@ except ImportError:
 from setuptools import setup
 
 install_reqs = [
-    'ply>=3.4',
-    'six>=1.3.0',
+    'ply >= 3.4',
+    'six >= 1.3.0',
 ]
 
 setup_requires = [
@@ -33,45 +33,41 @@ test_reqs = [
 with open('test/requirements.txt') as f:
     test_reqs += f.read().splitlines()
 
-with open('LICENSE') as f:
-    LICENSE = f.read()
-
 with open('README.rst') as f:
     README = f.read()
 
 dist = setup(
     name='stone',
     version='0.1',
-    description='Stone is an interface description language (IDL) for APIs.',
-    author='Ken Elkabany',
-    author_email='kelkabany@dropbox.com',
-    maintainer='Dropbox',
-    maintainer_email='dev-platform@dropbox.com',
-    url='https://github.com/dropbox/stone',
     install_requires=install_reqs,
     setup_requires=setup_requires,
     tests_require=test_reqs,
-    license=LICENSE,
-    zip_safe=False,
+    entry_points={
+        'console_scripts': ['stone=stone.cli:main'],
+    },
     packages=['stone',
               'stone.lang',
               'stone.target',
               'stone.target.python_rsrc'],
+    zip_safe=False,
+    author_email='kelkabany@dropbox.com',
+    author='Ken Elkabany',
+    description='Stone is an interface description language (IDL) for APIs.',
+    license='MIT License',
     long_description=README,
-    platforms=['CPython 2.7', 'CPython 3.4', 'CPython 3.5'],
-    entry_points={
-        'console_scripts': ['stone=stone.cli:main'],
-    },
+    maintainer_email='dev-platform@dropbox.com',
+    maintainer='Dropbox',
+    url='https://github.com/dropbox/stone',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Code Generators',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
