8f9d902e2be95d31a93c61ccd1be7e818a1471c4
Amritansh Gupta <amritanshg@dropbox.com>
2019-08-07 16:14:03 -0700

    Start with Basic Generator

---
+++ stone/backends/proto_gen/proto_client.stoneg.py
@@ -0,0 +1,16 @@
+from stone.backend import CodeBackend
+from stone.ir import(
+    Struct,
+    StructField,
+)
+
+class ProtoBackend(CodeBackend):
+    def generate(self, api):
+        with self.output_to_relative_path('test.proto'):
+            for namespace in api.namespaces.values():
+                self._create_package(namespace.name)
+                #self._generate_messages(namespace)
+
+    def _create_package(self, val):
+        self.emit("package " + val + ";")
+        self.emit()
