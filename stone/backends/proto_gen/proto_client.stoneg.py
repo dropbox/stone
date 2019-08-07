a6c9b13ec19a382f627082f8771e0aa0111e7f5b
Amritansh Gupta <amritanshg@dropbox.com>
2019-08-07 16:47:06 -0700

    Fix typo

--- stone/backends/proto_gen/proto_client.stoneg.py
+++ stone/backends/proto_gen/proto_client.stoneg.py
@@ -6,7 +6,6 @@ class ProtoBackend(CodeBackend):
         with self.output_to_relative_path('test.proto'):
             for namespace in api.namespaces.values():
                 self._create_package(namespace.name)
-                #self._generate_messages(namespace)
 
     def _create_package(self, val):
         self.emit("package " + val + ";")
