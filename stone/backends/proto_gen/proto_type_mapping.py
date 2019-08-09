1805d5a20d3637ecbf25065c1338bb182523cc43
Amritansh Gupta <amritanshg@dropbox.com>
2019-08-09 10:29:32 -0700

    Basic Message expression

--- stone/backends/proto_gen/proto_type_mapping.py
+++ stone/backends/proto_gen/proto_type_mapping.py
@@ -10,15 +10,10 @@ from stone.ir import (
     # is_alias,
     is_boolean_type,
     is_bytes_type,
-    # is_float_type,
-    # is_integer_type,
     is_float32_type,
     is_float64_type,
     is_int32_type,
     is_int64_type,
-    # is_list_type,
-    # is_map_type,
-    # is_nullable_type,
     is_string_type,
     is_timestamp_type,
     # is_user_defined_type,
@@ -28,22 +23,22 @@ from stone.ir import (
 def map_stone_type_to_proto(data_type):
 
     if is_string_type(data_type):
-        return 'string'
+        return u'string'
 
     elif is_boolean_type(data_type):
-        return 'bool'
+        return u'bool'
 
     elif is_int32_type(data_type):
-        return 'int32'
+        return u'int32'
 
     elif is_int64_type(data_type):
-        return 'int64'
+        return u'int64'
 
     elif is_float32_type(data_type):
-        return 'float32'
+        return u'float'
 
     elif is_float64_type(data_type):
-        return 'float64'
+        return u'double'
 
     elif is_bytes_type(data_type):
         return 'bytes'
