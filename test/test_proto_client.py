696b048bedf0c9aab85c89acb628f7d3e10cec9d
Amritansh Gupta <amritanshg@dropbox.com>
2019-08-15 12:28:09 -0700

    Removed nesting

--- test/test_proto_client.py
+++ test/test_proto_client.py
@@ -29,6 +29,14 @@ class TestGeneratedProtoClient(unittest.TestCase):
             args=None
         )
 
+    def test_package(self):
+        ns = ApiNamespace('TestPkg')
+        self.backend._create_package(ns.name)
+        res = self.backend.output_buffer_to_string()
+        expected = textwrap.dedent('package TestPkg;\n\n')
+
+        self.assertEqual(res, expected)
+
     def test_nested_struct(self):
 
         nest_struc = Struct(u'Name', "TestStruct", None)
@@ -49,19 +57,19 @@ class TestGeneratedProtoClient(unittest.TestCase):
         ])
 
         ns = ApiNamespace('TestStruct')
-        ns.add_data_type(parent_struc)
         ns.add_data_type(nest_struc)
+        ns.add_data_type(parent_struc)
 
         self.backend._generate_types(ns)
         res = self.backend.output_buffer_to_string()
 
         expected = textwrap.dedent('''\
-            message Person {
-                message Name {
-                    string\tgiven_name = 0;
-                    string\tsurname = 1;
-                }
+            message Name {
+                string\tgiven_name = 0;
+                string\tsurname = 1;
+            }
 
+            message Person {
                 Name\tname = 0;
                 uint64\tage = 1;
             }
@@ -105,12 +113,70 @@ class TestGeneratedProtoClient(unittest.TestCase):
 
         self.assertEqual(res, expected)
 
+    def test_struct_multiple_comp(self):
+        name_struc = Struct(u'Name', 'TestStruct', None)
+        name_struc.set_attributes(None,
+        [
+            StructField(u'given_name', String(), None, None),
+            StructField(u'surname', String(), None, None)
+        ])
 
-    def test_package(self):
-        ns = ApiNamespace('TestPkg')
-        self.backend._create_package(ns.name)
+        dob_struc = Struct(u'DOB', 'TestStruct', None)
+        dob_struc.set_attributes(None,
+        [
+            StructField(u'month', Int32(), None, None),
+            StructField(u'day', Int32(), None, None),
+            StructField(u'year', Int32(), None, None)
+        ])
+
+        person1_struc = Struct(u'Person1', "TestStruct", None)
+
+        person1_struc.set_attributes(None,
+        [
+            StructField(u'name',name_struc, None, None),
+            StructField(u'age', UInt64(), None, None)
+        ])
+
+        person2_struc = Struct(u'Person2', "TestStruct", None)
+
+        person2_struc.set_attributes(None,
+        [
+            StructField(u'name',name_struc, None, None),
+            StructField(u'dob', dob_struc, None, None)
+        ])
+
+        ns = ApiNamespace('TestStruct')
+        ns.add_data_type(dob_struc)
+        ns.add_data_type(name_struc)
+        ns.add_data_type(person1_struc)
+        ns.add_data_type(person2_struc)
+
+        self.backend._generate_types(ns)
         res = self.backend.output_buffer_to_string()
-        expected = textwrap.dedent('package TestPkg;\n\n')
+
+        expected = textwrap.dedent('''\
+           message DOB {
+               int32\tmonth = 0;
+               int32\tday = 1;
+               int32\tyear = 2;
+           }
+
+           message Name {
+               string\tgiven_name = 0;
+               string\tsurname = 1;
+           }
+
+           message Person1 {
+               Name\tname = 0;
+               uint64\tage = 1;
+           }
+
+           message Person2 {
+               Name\tname = 0;
+               DOB\tdob = 1;
+           }
+
+        ''')
 
         self.assertEqual(res, expected)
 unittest.main()
