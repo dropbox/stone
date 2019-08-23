55f2748359f8612df5879a7a6d4c529312225ecd
Amritansh Gupta <amritanshg@dropbox.com>
2019-08-23 10:49:19 -0700

    Update move

--- stone/backends/proto_client.py
+++ stone/backends/proto_client.py
@@ -1,27 +1,11 @@
-from __future__ import unicode_literals
 from stone.backend import CodeBackend
 
-import importlib
-argparse = importlib.import_module(str('argparse'))
 
-
-_cmdline_parser = argparse.ArgumentParser(prog='proto-client-backend')
-_cmdline_parser.add_argument(
-    '-m',
-    '--module-name',
-    required=True,
-    type=str,
-    help=('The name(without extension) of the protobuf file to generate.')
-)
 class ProtoBackend(CodeBackend):
-
-    cmdline_parser = _cmdline_parser
-
     def generate(self, api):
-        with self.output_to_relative_path('%s.proto' % self.args.module_name):
+        with self.output_to_relative_path('test.proto'):
             for namespace in api.namespaces.values():
                 self._create_package(namespace.name)
 
     def _create_package(self, val):
-        self.emit('syntax = "proto3";')
-        self.emit('package ' + val + ';')
+        self.emit("package " + val + ";")
--- stone/backends/proto_type_mapping.py
+++ stone/backends/proto_type_mapping.py
@@ -1,60 +1,56 @@
 from stone.ir import (
-    # Alias,
-    # ApiNamespace,
-    # DataType,
-    # List,
-    # Map,
-    # Nullable,
     Timestamp,
-    # UserDefined,
-    # is_alias,
     is_boolean_type,
     is_bytes_type,
-    # is_float_type,
-    # is_integer_type,
     is_float32_type,
     is_float64_type,
     is_int32_type,
     is_int64_type,
     is_uint32_type,
     is_uint64_type,
-    # is_list_type,
-    # is_map_type,
-    # is_nullable_type,
     is_string_type,
     is_timestamp_type,
-    # is_user_defined_type,
-    # is_void_type,
+    is_void_type,
+    Primitive
 )
 
 def map_stone_type_to_proto(data_type):
 
     if is_string_type(data_type):
-        return 'string'
+        return u'string'
 
     elif is_boolean_type(data_type):
-        return 'bool'
+        return u'bool'
 
     elif is_int32_type(data_type):
-        return 'int32'
+        return u'int32'
 
     elif is_int64_type(data_type):
-        return 'int64'
+        return u'int64'
 
     elif is_uint32_type(data_type):
         return 'uint32'
 
     elif is_uint64_type(data_type):
-         return 'uint64'
+        return 'uint64'
 
     elif is_float32_type(data_type):
-        return 'float32'
+        return u'float'
 
     elif is_float64_type(data_type):
-        return 'float64'
+        return u'double'
 
     elif is_bytes_type(data_type):
         return 'bytes'
 
     elif is_timestamp_type(data_type):
-        return 'timestamp'
+        return 'Timestamp'
+
+    elif is_void_type(data_type):
+        return "google.protobuf.Empty"
+        
+    else:
+        return None
+
+def is_primitive_data(data_type):
+    return isinstance(data_type, Primitive)
