b16fd415ebac29da493cfd2142bebd3ac0b89d44
Connor Worley <connorbworley@gmail.com>
2020-06-12 18:49:52 -0700

    Bump to 1.3.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('README.rst') as f:
 
 dist = setup(
     name='stone',
-    version='1.0.0',
+    version='1.3.0',
     install_requires=install_reqs,
     setup_requires=setup_requires,
     tests_require=test_reqs,
