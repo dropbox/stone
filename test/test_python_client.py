45770ebfe16485d3b8b0c23e015d9f4150431a9d
Scott Erickson <sderickson@gmail.com>
2022-01-18 10:00:44 -0800

    Add attrs export to python client (#262)
    Update the python client generator, adding a command line arg to export route attributes to the documentation string. This way generated docs can include specified attributes, in our case the scope attribute.

--- test/test_python_client.py
+++ test/test_python_client.py
@@ -10,6 +10,8 @@ from stone.ir import (
     Nullable,
     String,
     Void,
+    StructField,
+    Struct,
 )
 
 MYPY = False
@@ -27,7 +29,8 @@ class TestGeneratedPythonClient(unittest.TestCase):
 
         backend = PythonClientBackend(
             target_folder_path='output',
-            args=['-m', 'files', '-c', 'DropboxBase', '-t', 'dropbox'])
+            args=['-a', 'scope', '-a', 'another_attribute',
+                  '-m', 'files', '-c', 'DropboxBase', '-t', 'dropbox'])
         backend._generate_routes(ns)
         return backend.output_buffer_to_string()
 
@@ -217,5 +220,115 @@ class TestGeneratedPythonClient(unittest.TestCase):
                          'Nullable[str]')
         self.assertEqual(backend._format_type_in_doc(ns, Map(String(), Int32())),
                          'Map[str, int]')
+
+    def test_route_with_attributes_in_docstring(self):
+        # type: () -> None
+
+        route = ApiRoute('get_metadata', 1, None)
+        route.set_attributes(None, None, Void(), Void(), Void(), {
+            'scope': 'events.read', 'another_attribute': 'foo'
+        })
+        ns = ApiNamespace('files')
+        ns.add_route(route)
+
+        result = self._evaluate_namespace(ns)
+        expected = textwrap.dedent('''\
+            def files_get_metadata(self):
+                """
+                Route attributes:
+                    scope: events.read
+                    another_attribute: foo
+
+                :rtype: None
+                """
+                arg = None
+                r = self.request(
+                    files.get_metadata,
+                    'files',
+                    arg,
+                    None,
+                )
+                return None
+
+        ''')
+        self.assertEqual(result, expected)
+
+    def test_route_with_attributes_and_doc_in_docstring(self):
+        # type: () -> None
+        """
+        In particular make sure there's spacing b/w overview and attrs.
+        """
+
+        route = ApiRoute('get_metadata', 1, None)
+        route.set_attributes(None, "Test string.", Void(), Void(), Void(),
+                             {'scope': 'events.read'})
+        ns = ApiNamespace('files')
+        ns.add_route(route)
+
+        result = self._evaluate_namespace(ns)
+        expected = textwrap.dedent('''\
+            def files_get_metadata(self):
+                """
+                Test string.
+
+                Route attributes:
+                    scope: events.read
+
+                :rtype: None
+                """
+                arg = None
+                r = self.request(
+                    files.get_metadata,
+                    'files',
+                    arg,
+                    None,
+                )
+                return None
+
+        ''')
+        self.assertEqual(result, expected)
+
+    def test_route_with_doc_and_attribute_and_data_types(self):
+        # type: () -> None
+        ns = ApiNamespace('files')
+        struct = Struct('MyStruct', ns, None)
+        struct.set_attributes(None, [
+            StructField('field1', Int32(), None, None),
+            StructField('field2', Int32(), None, None),
+        ])
+
+        route = ApiRoute('test/route', 1, None)
+        route.set_attributes(
+            None, "Test string.", struct, Int32(), Void(), {'scope': 'events.read'}
+        )
+        ns.add_route(route)
+
+        result = self._evaluate_namespace(ns)
+        expected = textwrap.dedent('''\
+            def files_test_route(self,
+                                 field1,
+                                 field2):
+                """
+                Test string.
+
+                Route attributes:
+                    scope: events.read
+
+                :type field1: int
+                :type field2: int
+                :rtype: int
+                """
+                arg = files.MyStruct(field1,
+                                     field2)
+                r = self.request(
+                    files.test_route,
+                    'files',
+                    arg,
+                    None,
+                )
+                return r
+
+        ''')
+        self.assertEqual(result, expected)
 
     # TODO: add more unit tests for client code generation
