d593ae501ce1831416dad853de7de8b224b429bf
Roland Hui <rhui@dropbox.com>
2024-05-09 14:33:14 -0700

    fixing linter message string length issues

--- stone/backends/python_rsrc/stone_validators.py
+++ stone/backends/python_rsrc/stone_validators.py
@@ -93,9 +93,9 @@ def generic_type_name(v):
 
 def get_value_string(v, max_length=1000):
     # type: (typing.Any, int) -> String
-    """Return a truncated version of the input string.  If the input string is longer than 1000 characters, this will
-       return the first 1000 characters and append with '...The string has been truncated due to its length' to indicate
-       that it has been truncated."""
+    """Return a truncated version of the input string.  If the input string is longer than
+       1000 characters, this will return the first 1000 characters and append with '...The
+       string has been truncated due to its length' to indicate that it has been truncated."""
     v_str = str(v)
     if len(v_str) > max_length:
         return v_str[:max_length] + ' [The string has been truncated due to its length]'
